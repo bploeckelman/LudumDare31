22cd7a414d7f406d0b3e06ed34a41a16fdf70c56
Ian McNamara <ian.mcnamara@doit.wisc.edu>
2014-12-06 15:43:48 -0600

    Adding a GameObject

---
+++ core/src/lando/systems/ld31/GameObject.java
@@ -0,0 +1,9 @@
+package lando.systems.ld31;
+
+/**
+ * Author: Ian McNamara <ian.mcnamara@wisc.edu>
+ * Teaching and Research Application Development
+ * Copyright 2014 Board of Regents of the University of Wisconsin System
+ */
+public class GameObject {
+}
