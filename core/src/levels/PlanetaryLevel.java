b60e7aa22eeb614ffcaf7c3a05c0963ec4bf34fe
Ian McNamara <ian.mcnamara@doit.wisc.edu>
2014-12-06 14:45:35 -0600

    Desktop launcher now is configured to run at 1024x768

---
+++ core/src/levels/PlanetaryLevel.java
@@ -0,0 +1,9 @@
+package levels;
+
+/**
+ * Author: Ian McNamara <ian.mcnamara@wisc.edu>
+ * Teaching and Research Application Development
+ * Copyright 2014 Board of Regents of the University of Wisconsin System
+ */
+public class PlanetaryLevel {
+}
--- desktop/src/lando/systems/ld31/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/ld31/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package lando.systems.ld31.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld31.GameConstants;
 import lando.systems.ld31.LudumDare31;
 
 public class DesktopLauncher {
@@ -9,6 +10,8 @@ public class DesktopLauncher {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Ludum Dare 31 entry by Lando Systems";
         config.resizable = false;
+        config.width = GameConstants.ScreenWidth;
+        config.height = GameConstants.ScreenHeight;
         new LwjglApplication(new LudumDare31(), config);
     }
 }
