a98173efefdb5a8a89183c91749d9531831f5b22
Ian McNamara <ian.mcnamara@doit.wisc.edu>
2014-12-06 15:49:11 -0600

    updating level

--- core/src/levels/PlanetaryLevel.java
+++ core/src/levels/PlanetaryLevel.java
@@ -1,9 +1,39 @@
 package levels;
 
-/**
- * Author: Ian McNamara <ian.mcnamara@wisc.edu>
- * Teaching and Research Application Development
- * Copyright 2014 Board of Regents of the University of Wisconsin System
- */
-public class PlanetaryLevel {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import levels.planetary.Earth;
+
+
+public class PlanetaryLevel extends GameLevel {
+
+    private Earth earth;
+
+
+    public PlanetaryLevel() {
+        earth = new Earth(50, 30, 0);
+    }
+
+
+    @Override
+    public boolean hasThreat() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void handleInput(float dt) {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        earth.draw(batch);
+    }
+
 }
