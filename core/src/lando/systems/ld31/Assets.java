2a6afb1b6bfc228aa5c44523c0516577eb8cefb8
Doug Graham <dsgraham@gmail.com>
2014-12-06 11:23:24 -0600

    adding random

--- core/src/lando/systems/ld31/Assets.java
+++ core/src/lando/systems/ld31/Assets.java
@@ -1,5 +1,7 @@
 package lando.systems.ld31;
 
+import java.util.Random;
+
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,6 +13,7 @@ public class Assets {
 
     // ----- Textures ---------------------------
     public static Texture ludumdare;
+    public static Random rand;
 
 
     // ----- Audio ------------------------------
@@ -21,6 +24,7 @@ public class Assets {
 
     public static void load() {
         ludumdare = new Texture("ludumdare.png");
+        rand = new Random();
     }
 
     public static void dispose() {
