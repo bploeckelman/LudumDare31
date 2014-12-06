2a5c05fb2a69c669ad0e73b1f33f0b8f5ec415f5
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-12-06 10:50:22 -0600

    tabs to spaces

--- core/src/lando/systems/ld31/LudumDare31.java
+++ core/src/lando/systems/ld31/LudumDare31.java
@@ -13,64 +13,64 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LudumDare31 extends ApplicationAdapter {
-	TweenManager tweens;
-	SpriteBatch batch;
-	Texture img;
-	float x, y;
-//	float r, g, b;
-	MutableFloat r, g, b;
-	float accum;
-	float threshold;
-	int xdir, ydir;
+    TweenManager tweens;
+    SpriteBatch batch;
+    Texture img;
+    float x, y;
+//    float r, g, b;
+    MutableFloat r, g, b;
+    float accum;
+    float threshold;
+    int xdir, ydir;
 
-	final float speed = 50f;
+    final float speed = 50f;
 
-	@Override
-	public void create () {
-		tweens = new TweenManager();
-		batch = new SpriteBatch();
-		img = new Texture("ludumdare.png");
-		x = Gdx.graphics.getWidth()  / 2f - img.getWidth()  / 2f;
-		y = Gdx.graphics.getHeight() / 2f - img.getHeight() / 2f;
-		r = new MutableFloat(1);
-		g = new MutableFloat(1);
-		b = new MutableFloat(1);
-		accum = 0;
-		threshold = 1;
-		xdir = 1;
-		ydir = 1;
-	}
+    @Override
+    public void create () {
+        tweens = new TweenManager();
+        batch = new SpriteBatch();
+        img = new Texture("ludumdare.png");
+        x = Gdx.graphics.getWidth()  / 2f - img.getWidth()  / 2f;
+        y = Gdx.graphics.getHeight() / 2f - img.getHeight() / 2f;
+        r = new MutableFloat(1);
+        g = new MutableFloat(1);
+        b = new MutableFloat(1);
+        accum = 0;
+        threshold = 1;
+        xdir = 1;
+        ydir = 1;
+    }
 
-	@Override
-	public void render () {
-		if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-			Gdx.app.exit();
-		}
+    @Override
+    public void render () {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
 
-		float delta = Gdx.graphics.getDeltaTime();
+        float delta = Gdx.graphics.getDeltaTime();
 
-		tweens.update(delta);
+        tweens.update(delta);
 
-		accum += delta;
-		if (accum > threshold) {
-			accum -= threshold;
-			Timeline.createParallel()
+        accum += delta;
+        if (accum > threshold) {
+            accum -= threshold;
+            Timeline.createParallel()
                     .push(Tween.to(r, 0, threshold).target((float) Math.random()).ease(Cubic.INOUT))
                     .push(Tween.to(g, 0, threshold).target((float) Math.random()).ease(Cubic.INOUT))
                     .push(Tween.to(b, 0, threshold).target((float) Math.random()).ease(Cubic.INOUT))
-					.start(tweens);
-		}
+                    .start(tweens);
+        }
 
-		x += xdir * delta * speed;
-		y += ydir * delta * speed;
-		if (x < 0 || x + img.getWidth()  > Gdx.graphics.getWidth())  xdir *= -1;
-		if (y < 0 || y + img.getHeight() > Gdx.graphics.getHeight()) ydir *= -1;
+        x += xdir * delta * speed;
+        y += ydir * delta * speed;
+        if (x < 0 || x + img.getWidth()  > Gdx.graphics.getWidth())  xdir *= -1;
+        if (y < 0 || y + img.getHeight() > Gdx.graphics.getHeight()) ydir *= -1;
 
-		Gdx.gl.glClearColor(r.floatValue(),g.floatValue(),b.floatValue(),1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(r.floatValue(),g.floatValue(),b.floatValue(),1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.begin();
-		batch.draw(img, x, y);
-		batch.end();
-	}
+        batch.begin();
+        batch.draw(img, x, y);
+        batch.end();
+    }
 }
--- desktop/src/lando/systems/ld31/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/ld31/desktop/DesktopLauncher.java
@@ -5,10 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import lando.systems.ld31.LudumDare31;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Ludum Dare 31 entry by Lando Systems";
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Ludum Dare 31 entry by Lando Systems";
         config.resizable = false;
-		new LwjglApplication(new LudumDare31(), config);
-	}
+        new LwjglApplication(new LudumDare31(), config);
+    }
 }
