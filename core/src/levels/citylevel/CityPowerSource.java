9fbbd7b338ff8310229b98abb13a89d5269aa027
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-12-07 17:05:13 -0600

    updates to city

--- core/src/levels/citylevel/CityPowerSource.java
+++ core/src/levels/citylevel/CityPowerSource.java
@@ -1,5 +1,7 @@
 package levels.citylevel;
 
+import java.util.Stack;
+
 /**
  * Brian Ploeckelman created on 12/6/2014.
  */
@@ -8,9 +10,34 @@ public class CityPowerSource {
     public int x, y;
     public boolean connected;
 
+    private Stack<PowerTile> powerTileStack;
+
     public CityPowerSource(int x, int y) {
         this.x = x;
         this.y = y;
+        this.powerTileStack = new Stack<PowerTile>();
     }
+
+    public void energizeConnections(PowerTile[][] powerTiles) {
+        powerTileStack.clear();
+        powerTileStack.push(powerTiles[y][x]);
+        while (!powerTileStack.isEmpty()) {
+            PowerTile top = powerTileStack.pop();
+            if (!top.visited) {
+                // Have we reached the bar node?
+                if (top.isBar) break;
+
+                top.visited = true;
+                top.energized = true;
+
+                // Add connected neighbors to stack
+                if (top.up    != null) powerTileStack.push(powerTiles[top.up.y   ][top.up.x   ]);
+                if (top.down  != null) powerTileStack.push(powerTiles[top.down.y ][top.down.x ]);
+                if (top.left  != null) powerTileStack.push(powerTiles[top.left.y ][top.left.x ]);
+                if (top.right != null) powerTileStack.push(powerTiles[top.right.y][top.right.x]);
+            }
+        }
+    }
+
 
 }
